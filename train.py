e68db13db519f98786acca0a5307fb8ba1403d7f
glenn-jocher <glenn.jocher@ultralytics.com>
2024-01-01 02:05:19 +0000

    Auto-format by Ultralytics actions

--- train.py
+++ train.py
@@ -5,10 +5,10 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-np.set_printoptions(linewidth=320, formatter={'float_kind': '{:11.5g}'.format})  # format short g, %precision=5
-matplotlib.rc('font', **{'size': 8})
+np.set_printoptions(linewidth=320, formatter={"float_kind": "{:11.5g}".format})  # format short g, %precision=5
+matplotlib.rc("font", **{"size": 8})
 
-path = '/Users/glennjocher/Downloads/SANDD/'
+path = "/Users/glennjocher/Downloads/SANDD/"
 
 
 # 1. time each waveform, and then integrate based on a new t0 which is the timed sample.
@@ -16,15 +16,18 @@ path = '/Users/glennjocher/Downloads/SANDD/'
 # 3. try and make a flat fielding map from all the data
 # 4. Slava will make a new format with rod_ID
 
+
 def main():
-    files = ['background_64rods_noteflon_amps_27V_12hr_0.glenn',  # background
-             'Cs_collimated_oneSource_center_64rods_noteflon_amps_27V_500s_0.glenn',  # Cs137 (gammas)
-             'Cf_centered_20inches_from6inchLeadShield_64rods_noteflon_amps_27V_500s_0.glenn']  # Cf (gammas + neutrons)
+    files = [
+        "background_64rods_noteflon_amps_27V_12hr_0.glenn",  # background
+        "Cs_collimated_oneSource_center_64rods_noteflon_amps_27V_500s_0.glenn",  # Cs137 (gammas)
+        "Cf_centered_20inches_from6inchLeadShield_64rods_noteflon_amps_27V_500s_0.glenn",
+    ]  # Cf (gammas + neutrons)
 
     # Read data
     # timestamp, digitizer_ID, digitizer_channel, SiPM_ID, SiPM_channnel, number_of_samples, V1 V2 V3 .... V400
     file = files[1]
-    with open(path + file, 'r') as f:  # Method 3 (5.5s)
+    with open(path + file, "r") as f:  # Method 3 (5.5s)
         lines = f.read().split()
     # x = np.array(lines[:406 * 3000], dtype=np.float32).reshape((-1, 406))
     x = np.array(lines, dtype=np.float32).reshape((-1, 406))
@@ -48,7 +51,7 @@ def main():
 
     # Candidate cuts
     j = (ymax.ravel() < 9000) & (ymax.ravel() > 100) & (yn.min(1) > -0.5) & (yn[:, :50].std(1) < 0.1) & (q_tail > 0)
-    print('%g/%g (%.1f%%) pass candidate cuts' % (j.sum(), len(y), j.mean() * 100))
+    print("%g/%g (%.1f%%) pass candidate cuts" % (j.sum(), len(y), j.mean() * 100))
     y, q_total, q_tail = y[j], q_total[j], q_tail[j]
 
     # Plotting
@@ -56,7 +59,7 @@ def main():
     fig = plt.figure(figsize=(7, 7))
     plt.subplot(2, 2, 1)
     plt.plot(y[:30].T)
-    plt.autoscale(axis='both', tight=True)
+    plt.autoscale(axis="both", tight=True)
     plt.suptitle(file)
 
     plt.subplot(2, 2, 2)
@@ -64,21 +67,21 @@ def main():
     r = q_tail / q_total
     i = (r > 0.5) & (r < 0.8) & (q_total < 200) & (q_total > 10)
     cmap = plt.cm.jet
-    cmap.set_under('w', 1)
-    plt.hist2d(q_total[i], r[i], bins=n, range= [[0, 200], [0.5, 0.8]], cmap=cmap, vmin=1)
+    cmap.set_under("w", 1)
+    plt.hist2d(q_total[i], r[i], bins=n, range=[[0, 200], [0.5, 0.8]], cmap=cmap, vmin=1)
 
     plt.subplot(2, 2, 3)
     plt.hist(r[i & (q_total > 20)], bins=n, log=log)
-    plt.title('Q_tail / Q_total')
+    plt.title("Q_tail / Q_total")
 
     plt.subplot(2, 2, 4)
     plt.hist(q_total[i], bins=n, range=[0, 200], log=log)
-    plt.title('Q_total')
+    plt.title("Q_total")
 
     fig.tight_layout()
-    fig.savefig('results.png', dpi=300)
-    if platform == 'darwin':  # MacOS
-        os.system('open results.png')
+    fig.savefig("results.png", dpi=300)
+    if platform == "darwin":  # MacOS
+        os.system("open results.png")
 
     # Plot.ly
     # trace = go.Scatter(y=y.T)
@@ -102,13 +105,13 @@ def charges(x, s=100):
     n = len(s)
     q_tail, q_total = np.zeros((2, n))
     for i in range(n):
-        a = x[i, s[i] + 0: s[i] + 220]
+        a = x[i, s[i] + 0 : s[i] + 220]
         q_tail[i] = a[28:].sum()
         q_total[i] = a.sum()
 
     # q_total = x[:, s + 0: s + 220].sum(1)
     # q_tail = x[:, s + 28: s + 220].sum(1)
-    return q_total/1000, q_tail/1000
+    return q_total / 1000, q_tail / 1000
 
 
 def matched_sipms(array_id):
@@ -117,5 +120,5 @@ def matched_sipms(array_id):
     #
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
